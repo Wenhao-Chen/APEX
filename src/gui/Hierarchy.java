38ea33a13949f2ecea6b8c3d9bc7f395c54219a5
xjtujack <xjtujack@gmail.com>
2016-03-17 23:32:06 -0500

    save

--- src/gui/Hierarchy.java
+++ src/gui/Hierarchy.java
@@ -10,6 +10,12 @@ import org.w3c.dom.NodeList;
 
 public class Hierarchy {
 
+	private Node hierarchy;
+	private int rotation;	// 0 - portrait, 1 - landscape
+	
+	
+	
+	
 	/**
 	 * Create Hierarchy object from window_dump.xml by uiautomator.
 	 * 
@@ -24,13 +30,15 @@ public class Hierarchy {
 		try
 		{
 			Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(hiearchyXML);
-			NodeList nodes = doc.getChildNodes();
+			hierarchy = doc.getFirstChild();
+			rotation = Integer.parseInt(hierarchy.getAttributes().getNamedItem("rotation").getTextContent());
+			NodeList nodes = hierarchy.getChildNodes();
 			for (int i = 0; i < nodes.getLength(); i++)
 			{
 				Node node = nodes.item(i);
-				System.out.println(node);
+				System.out.println(node.getNodeName());
+				System.out.println(node.getAttributes());
 			}
-			
 		}
 		catch (Exception e)
 		{
